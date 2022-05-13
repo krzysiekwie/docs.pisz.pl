e0885025e1949fb72bba66636d342dca78e89a1c
krzysiekwie <54991735+krzysiekwie@users.noreply.github.com>
2022-05-13 17:41:48 +0200

    personalization

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'docs.pisz.pl'
+copyright = '2022, Krzysztof Wierzbicki'
+author = 'Krzysztof Wierzbicki'
 
 release = '0.1'
 version = '0.1.0'
